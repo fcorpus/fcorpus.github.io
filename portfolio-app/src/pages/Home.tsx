b75aa7c2c641633fc9ab786bae8c3d97c5a8f0b1
Francisco Castillo <fcastillocorpus@gmail.com>
2026-01-06 12:05:12 -0600

    feat: config of taildwind + basic routing

---
+++ portfolio-app/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+
+export const Home = () => {
+    return <div className="min-h-screen ">
+
+        {/* Theme toggle*/}
+
+        {/* Backgroyd effects*/}
+        
+        {/* Navbar */}
+        
+        {/* Main Content */}
+        
+        {/* Footer */}
+    
+    </div>;
+}
